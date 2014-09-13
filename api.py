0b44ff6bb4b525ffe9adbb0d33c7cae778a1525b
Eric Larssen <eric.larssen@workiva.com>
2014-09-13 16:03:29 -0500

    Everything!!!

--- api.py
+++ api.py
@@ -1,7 +1,20 @@
 import json
 import webapp2
-
+from google.appengine.api import users
 import models
+
+class BoardHandler(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if not user:
+            self.response.set_status(403)
+            return
+        email = user.email()
+        board = models.Board.get_by_id(email)
+        if not board:
+            board = models.Board(id=email)
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(json.dumps(board.to_json_dict()))
 
 
 class CardHandler(webapp2.RequestHandler):
@@ -15,5 +28,6 @@ class CardHandler(webapp2.RequestHandler):
         self.response.out.write(json.dumps(output))
 
 endpoints = [
+    ('/api/boards', BoardHandler),
     ('/api/cards', CardHandler)
 ]
--- static/js/app.jsx
+++ static/js/app.jsx
@@ -4,17 +4,29 @@
 var React = window.React = require('react'),
     Navbar = require('./components/navbar.jsx'),
     Board = require('./components/board.jsx'),
-    mountNode = document.getElementById('postBox');
+    mountNode = document.getElementById('postBox'),
+    $ = require('jquery');
 
 var App = React.createClass({
-  render: function() {
-    return (
-        <div>
-          <Navbar/>
-          <Board data={this.props.items}/>
-        </div>
-    );
-  }
+    getInitialState: function() {
+        return {data: []};
+    },
+    componentDidMount: function(){
+        this.fetchBoards();
+    },
+    fetchBoard: function(){
+        $.getJSON('/',function(){
+
+        });
+    },
+    render: function() {
+        return (
+            <div>
+              <Navbar/>
+              <Board data={this.props.items}/>
+            </div>
+        );
+    }
 });
 
 var ITEMS = [
@@ -36,4 +48,4 @@ var ITEMS = [
     }
 ];
 
-React.renderComponent(<App items={ITEMS}/>, mountNode);
+React.renderComponent(<App/>, mountNode);

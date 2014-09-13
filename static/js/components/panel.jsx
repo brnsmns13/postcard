ad81f34f2a5423a279d7e04d9c01aacb676150be
Eric Larssen <eric.larssen@workiva.com>
2014-09-13 13:54:26 -0500

    Made Stuff

--- static/js/components/panel.jsx
+++ static/js/components/panel.jsx
@@ -9,15 +9,30 @@ var PostCardPanel = React.createClass({
     getInitialState: function() {
         return {data: this.props.data};
     },
+
+    allowDrop: function(ev) {
+        ev.preventDefault();
+    },
+
+    drop: function(ev) {
+        ev.preventDefault();
+        var droppedItem = JSON.parse(ev.dataTransfer.getData("draggedItem"));
+        var sourcePanel = JSON.parse(ev.dataTransfer.getData("sourcePanel"));
+        this.props.onUserDrop(this.props.id, droppedItem, sourcePanel);
+    },
+
     render: function() {
-        var cards = {};
-        this.state.data.forEach(function(data) {
-            cards['card-'+data.id] = <Card text={data.text}/>
-        });
+        var rows = []
+        var lastCategory = null;
+        if (this.state.data) {
+            this.state.data.map(function(item, i) {
+                rows.push(<Card item={item} panel={this.props.title}/>)
+            }.bind(this));
+        }
         return (
             <div className="col-md-3">
-                <Panel header={this.props.title}>
-                    {cards}
+                <Panel onDragOver={this.allowDrop} onDrop={this.drop} header={this.props.title}>
+                    {rows}
                 </Panel>
             </div>
         );

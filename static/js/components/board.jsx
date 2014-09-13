d99214f9a00481b81c9ea92d5fdd47f25799aa52
Eric Larssen <eric.larssen@workiva.com>
2014-09-13 02:34:28 -0500

    Created Panels in React

--- static/js/components/board.jsx
+++ static/js/components/board.jsx
@@ -1,13 +1,18 @@
 /** @jsx React.DOM */
 'use strict';
 
-var React = window.React = require('react');
+var React = window.React = require('react'),
+    Panel = require('./panel.jsx');
 
 var Board = React.createClass({
   render: function() {
     return (
       <div className="container-fluid board">
-
+        <div className="row equal">
+            <Panel title="Todo"/>
+            <Panel title="In Progress"/>
+            <Panel title="Done"/>
+        </div>
       </div>
     );
   }

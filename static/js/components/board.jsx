434b35b56633a6d5c0de9bf1591fb69b0dd7ecb2
Eric Larssen <eric.larssen@workiva.com>
2014-09-13 07:39:08 -0500

    Added Modals and Buttons for adding Cards and Panels

--- static/js/components/board.jsx
+++ static/js/components/board.jsx
@@ -6,11 +6,12 @@ var React = window.React = require('react'),
 
 var Board = React.createClass({
   render: function() {
+    var data = [{id:'0', text:'test0'}, {id:'1', text:'test1'}, {id:'2', text:'test2'}];
     return (
       <div className="container-fluid board">
-            <PostCardPanel title="Todo"/>
-            <PostCardPanel title="In Progress"/>
-            <PostCardPanel title="Done"/>
+            <PostCardPanel title="Todo" data={data}/>
+            <PostCardPanel title="In Progress" data={data}/>
+            <PostCardPanel title="Done" data={data}/>
       </div>
     );
   }
--- static/js/components/navbar.jsx
+++ static/js/components/navbar.jsx
@@ -1,14 +1,30 @@
 /** @jsx React.DOM */
 'use strict';
 
-var React = window.React = require('react');
+var React = window.React = require('react'),
+    Button = require('react-bootstrap/Button'),
+    ModalTrigger = require('react-bootstrap/ModalTrigger'),
+    CreateCardModal = require('./createCard.jsx'),
+    CreatePanelModal = require('./createPanel.jsx'),
+    ButtonToolbar = require('react-bootstrap/ButtonToolbar');
 
 var Navbar = React.createClass({
   render: function() {
     return (
       <div className="container-fluid">
           <div className="header">
-              <h3 className="brand">PostCard</h3>
+                <h2 className="brand">PostCard
+                    <ModalTrigger modal={<CreatePanelModal/>}>
+                        <ButtonToolbar className="pull-right">
+                            <Button bsSize="small">Add Panel</Button>
+                        </ButtonToolbar>
+                    </ModalTrigger>
+                    <ModalTrigger modal={<CreateCardModal/>}>
+                        <ButtonToolbar className="pull-right">
+                            <Button bsSize="small">Add Card</Button>
+                        </ButtonToolbar>
+                    </ModalTrigger>
+                </h2>
           </div>
       </div>
     );

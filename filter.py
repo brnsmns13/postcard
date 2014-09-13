4a4f22291b12033e6ea825437c0167e0f847d5b1
Kevin <simkev2@gmail.com>
2014-09-13 12:45:25 -0500

    Working on filter for incoming emails

---
+++ filter.py
@@ -0,0 +1,19 @@
+import json
+
+def get_tags(j):
+	filters = [{'type':'From', 'param':'@twitter.com', 'tag':'twitter'}, {'type':'To', 'param':'simkev2@gmail.com', 'tag':'todo'}]
+	tag_list = []
+	for i in j['payload']['headers']:
+		for f in filters:
+			if (i['name'] == f['type']):
+				print i['name'] + ': ' + i['value']
+				tag_list.append(f['tag'])
+
+	return tag_list
+
+
+if __name__ == '__main__':
+	json_data = open("email.json")
+	j = json.loads(json_data.read())
+
+	print get_tags(j)
